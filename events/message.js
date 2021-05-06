0963e7ffa09c2e371d4188f7bfad44ff588d4ea4
ali <51881653+alispw77@users.noreply.github.com>
2021-05-06 14:54:59 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -15,24 +15,18 @@ module.exports = (client, message) => {
     }
     else{
         if(message.content.indexOf('.') == 0)return;
-        var settings_channels = global.fullarr.channels;
-        var settings_game_bool = global.fullarr.game_bool;
-        var settings_son_kelime_yazan = global.fullarr.son_kelime_yazan;
-        var settings_son_kelime = global.fullarr.son_kelime;
-        var settings_kullanilan_kelimeler_guilds = global.fullarr.kullanilan_kelimeler_guilds;
-        var settings_puanlar_guilds = global.fullarr.puanlar;
-        var puanlar_guilds_index = settings_puanlar_guilds.findIndex(find => find.guild_id === message.guild.id);
-        var puanlar_client_index = settings_puanlar_guilds[puanlar_guilds_index].puanlar.findIndex(find => find.client_id === message.author.id);
-        let kanal_index = settings_channels.findIndex(find => find.guild_id === message.guild.id);
-        let game_bool_index = settings_game_bool.findIndex(find => find.guild_id === message.guild.id);
-        let son_kelime_index = settings_son_kelime.findIndex(find => find.guild_id === message.guild.id);
-        let son_kelime_yazan_index = settings_son_kelime_yazan.findIndex(find => find.guild_id === message.guild.id);
-        let kullanilan_kelimeler_guilds_index = settings_kullanilan_kelimeler_guilds.findIndex(find => find.guild_id === message.guild.id);
-        if(settings_game_bool[game_bool_index].game_bool == 'true' && settings_channels[kanal_index].channel_id == message.channel.id){
+        let kanal_index = global.fullarr.channels.findIndex(find => find.guild_id === message.guild.id);
+        let game_bool_index = global.fullarr.game_bool.findIndex(find => find.guild_id === message.guild.id);
+        if(global.fullarr.game_bool[game_bool_index].game_bool == 'true' && global.fullarr.channels[kanal_index].channel_id == message.channel.id){
+            var puanlar_guilds_index = global.fullarr.puanlar.findIndex(find => find.guild_id === message.guild.id);
+            var puanlar_client_index = global.fullarr.puanlar[puanlar_guilds_index].puanlar.findIndex(find => find.client_id === message.author.id);
+            let son_kelime_index = global.fullarr.son_kelime.findIndex(find => find.guild_id === message.guild.id);
+            let son_kelime_yazan_index = global.fullarr.son_kelime_yazan.findIndex(find => find.guild_id === message.guild.id);
+            let kullanilan_kelimeler_guilds_index = global.fullarr.kullanilan_kelimeler_guilds.findIndex(find => find.guild_id === message.guild.id);            
             let sonharf = message.content.length;
             let kelime = message.content.toLowerCase();
             sonharf = kelime.charAt(sonharf - 1);
-            if(settings_son_kelime_yazan[son_kelime_yazan_index].son_kelime_yazan == message.author.id){
+            if(global.fullarr.son_kelime_yazan[son_kelime_yazan_index].son_kelime_yazan == message.author.id){
                 const embed = new Discord.MessageEmbed()
                     .setAuthor(message.author.username, message.author.avatarURL(),"https://leaderclan.com")
                     .setDescription(`${message.author.username}, üst üste yazamazsınız.`)
@@ -40,8 +34,8 @@ module.exports = (client, message) => {
                 message.channel.send(embed).then(del => del.delete({timeout:2000} , message.delete({timeout:2000})));
                 return;
             }
-            if(settings_son_kelime[son_kelime_index].son_harf && settings_son_kelime[son_kelime_index].son_harf !== kelime[0]){
-                let stngssonharf = settings_son_kelime[son_kelime_index].son_harf;
+            if(global.fullarr.son_kelime[son_kelime_index].son_harf && global.fullarr.son_kelime[son_kelime_index].son_harf !== kelime[0]){
+                let stngssonharf = global.fullarr.son_kelime[son_kelime_index].son_harf;
                 const embed = new Discord.MessageEmbed()
                     .setAuthor(message.author.username, message.author.avatarURL(),"https://leaderclan.com")
                     .setDescription(`**${kelime}**, bu kelime **${stngssonharf}** ile başlamıyor.`)
@@ -49,7 +43,7 @@ module.exports = (client, message) => {
                 message.channel.send(embed).then(del => del.delete({timeout:2000} , message.delete({timeout:2000})));
                 return;
             }
-            if(settings_kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.includes(kelime)){
+            if(global.fullarr.kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.includes(kelime)){
                 const embed = new Discord.MessageEmbed()
                     .setAuthor(message.author.username, message.author.avatarURL(),"https://leaderclan.com")
                     .setDescription(`**${kelime}**, bu kelime daha önce kullanılmış.`)
@@ -65,8 +59,8 @@ module.exports = (client, message) => {
                 message.channel.send(embed).then(del => del.delete({timeout:2000} , message.delete({timeout:2000})));
                 return;                
             }
-            if(sonharf == 'ğ' && settings_kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.length < 1500){
-                let kalan_kelime = 1500 - settings_kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.length;
+            if(sonharf == 'ğ' && global.fullarr.kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.length < 1500){
+                let kalan_kelime = 1500 - global.fullarr.kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.length;
                 const embed = new Discord.MessageEmbed()
                     .setAuthor(message.author.username, message.author.avatarURL(),"https://leaderclan.com")
                     .setDescription(`Oyun bitirici kelimeleri yazmak için biraz daha oynamanız gerekmekte.\nKalan kelime sayısı **${kalan_kelime}**`)
@@ -98,7 +92,7 @@ module.exports = (client, message) => {
                             message.channel.send(embed).then(del => del.delete({timeout:2000} , message.delete({timeout:2000})));
                         return;
                     }
-                    if(settings_kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.includes(kelime)){
+                    if(global.fullarr.kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.includes(kelime)){
                         const embed = new Discord.MessageEmbed()
                             .setAuthor(message.author.username, message.author.avatarURL(),"https://leaderclan.com")
                             .setDescription(`**${kelime}**, bu kelime daha önce kullanılmış.`)
@@ -106,39 +100,39 @@ module.exports = (client, message) => {
                         message.channel.send(embed).then(del => del.delete({timeout:2000} , message.delete({timeout:2000})));
                         return;
                     }
-		            settings_kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.push(kelime);
+		            global.fullarr.kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.push(kelime);
                     message.react('✔️');
                     if(puanlar_client_index == -1){
-                        settings_puanlar_guilds[puanlar_guilds_index].puanlar.push({
+                        global.fullarr.puanlar[puanlar_guilds_index].puanlar.push({
                             client_id : message.author.id,
                             puan : 3
                         });
                     }else{
-                        settings_puanlar_guilds[puanlar_guilds_index].puanlar[puanlar_client_index].puan += 3;
+                        global.fullarr.puanlar[puanlar_guilds_index].puanlar[puanlar_client_index].puan += 3;
                     }
-                    settings_son_kelime_yazan[son_kelime_yazan_index].son_kelime_yazan = message.author.id;
-                    settings_son_kelime[son_kelime_index].son_kelime = kelime;
-                    settings_son_kelime[son_kelime_index].son_harf = sonharf;
-                    if(sonharf == 'ğ' && settings_kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.length >= 1500){
+                    global.fullarr.son_kelime_yazan[son_kelime_yazan_index].son_kelime_yazan = message.author.id;
+                    global.fullarr.son_kelime[son_kelime_index].son_kelime = kelime;
+                    global.fullarr.son_kelime[son_kelime_index].son_harf = sonharf;
+                    if(sonharf == 'ğ' && global.fullarr.kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler.length >= 1500){
                         const embed = new Discord.MessageEmbed()
                             .setAuthor(message.author.username, message.author.avatarURL(),"https://leaderclan.com")
                             .setDescription(`${message.author},  Kilit kelimeyi yazarak oyunu bitirdi tebrikler birazdan yeni oyun başlayacak <3.`)
                             .setColor("#FFFFFF");
                         message.channel.send(embed);
-                        settings_puanlar_guilds[puanlar_guilds_index].puanlar[puanlar_client_index].puan += 50;
-                        settings_game_bool[game_bool_index].game_bool = 'false';
+                        global.fullarr.puanlar[puanlar_guilds_index].puanlar[puanlar_client_index].puan += 50;
+                        global.fullarr.game_bool[game_bool_index].game_bool = 'false';
                         fs.writeFile(settings_path, JSON.stringify(global.fullarr, null, 2), 'utf-8', function(err) {
                             if (err) throw err;
                             setTimeout(() => {
-                                let rastgele_kelimeler = settings_kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler;
+                                let rastgele_kelimeler = global.fullarr.kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler;
                                 let sansli_isim = rastgele_kelimeler[Math.floor(Math.random() * (rastgele_kelimeler.length - 1))];
                                 let sonharf = sansli_isim.length;
                                 sonharf = sansli_isim.charAt(sonharf - 1);
-                                delete settings_son_kelime_yazan[son_kelime_yazan_index].son_kelime_yazan;
-                                settings_son_kelime[son_kelime_index].son_kelime = sansli_isim;
-                                settings_son_kelime[son_kelime_index].son_harf = sonharf;
-                                settings_kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler = [sansli_isim];
-                                settings_game_bool[game_bool_index].game_bool = 'true';
+                                delete global.fullarr.son_kelime_yazan[son_kelime_yazan_index].son_kelime_yazan;
+                                global.fullarr.son_kelime[son_kelime_index].son_kelime = sansli_isim;
+                                global.fullarr.son_kelime[son_kelime_index].son_harf = sonharf;
+                                global.fullarr.kullanilan_kelimeler_guilds[kullanilan_kelimeler_guilds_index].kullanilan_kelimeler = [sansli_isim];
+                                global.fullarr.game_bool[game_bool_index].game_bool = 'true';
                                 fs.writeFile(settings_path, JSON.stringify(global.fullarr, null, 2), 'utf-8', function(err) {
                                     if (err) throw err;
                                     const embed = new Discord.MessageEmbed()
